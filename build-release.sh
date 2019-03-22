9d0b10eae39de2d24283753480800d87ff3db709
cs8425 <cs8425@gmail.com>
2019-03-23 00:38:43 +0800

    update build script

--- build-release.sh
+++ build-release.sh
@@ -21,80 +21,97 @@ LDFLAGS="-X main.VERSION=$VERSION -s -w"
 GCFLAGS=""
 OUT="build"
 
-OSES=(linux darwin windows freebsd)
-ARCHS=(amd64 386)
-for os in ${OSES[@]}; do
-	for arch in ${ARCHS[@]}; do
-		suffix=""
-		if [ "$os" == "windows" ]
-		then
-			suffix=".exe"
-		fi
-		pushd client
-		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/client_${os}_${arch}${suffix} .
-		popd
+startgo() {
+NAME=$1
+shift
+case "$NAME" in
+'freebsd-amd64')
+	rungo freebsd amd64 7 "$@"
+;;
+'freebsd-386')
+	rungo freebsd 386 7 "$@"
+;;
 
-		pushd server
-		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/server_${os}_${arch}${suffix} .
-		popd
+'darwin-amd64')
+	rungo darwin amd64 7 "$@"
+;;
+'darwin-386')
+	rungo darwin 386 7 "$@"
+;;
 
-		pushd $OUT
-		if $UPX; then upx -9 client_${os}_${arch}${suffix} server_${os}_${arch}${suffix};fi
-		tar -zcf kcptun-${os}-${arch}-$VERSION.tar.gz client_${os}_${arch}${suffix} server_${os}_${arch}${suffix}
-		$sum kcptun-${os}-${arch}-$VERSION.tar.gz
-		popd
-	done
-done
+'amd64')
+	rungo linux amd64 7 "$@"
+;;
+'386')
+	rungo linux 386 7 "$@"
+;;
+'arm5')
+	rungo linux arm 5 "$@"
+;;
+'arm6')
+	rungo linux arm 6 "$@"
+;;
+'arm7')
+	rungo linux arm 7 "$@"
+;;
+'arm8' | 'arm64' | 'aarch64')
+	rungo linux arm64 7 "$@"
+;;
+'mipsle')
+	rungo linux mipsle 7 "$@"
+;;
+'mips')
+	rungo linux mips 7 "$@"
+;;
 
-# ARM
-ARMS=(5 6 7)
-for v in ${ARMS[@]}; do
+'windows-386')
+	rungo windows 386 7 "$@"
+;;
+'windows-amd64')
+	rungo windows amd64 7 "$@"
+
+esac
+}
+
+rungo() {
+	OS=$1
+	shift
+	ARCH=$1
+	shift
+	ARM=$1
+	shift
+	#echo "[$OS, $ARCH, $ARM]": "$@"
+	CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH GOARM=$ARM go "$@"
+}
+
+
+ARCHS=(amd64 386 darwin-amd64 darwin-386 windows-386 windows-amd64 freebsd-amd64 freebsd-386 arm8 arm7 arm6 arm5 mipsle mips)
+for v in ${ARCHS[@]}; do
+	suffix=""
+
+	arch=`echo $v|cut -d'-' -f2`
+	os=`echo $v|cut -d'-' -f1`
+	if [ "$os" == "$arch" ]
+	then
+		os="linux"
+	fi
+	if [ "$os" == "windows" ]
+	then
+		suffix=".exe"
+	fi
+
 	pushd client
-	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/client_linux_arm$v  .
+	startgo $v build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/client_${os}_${arch}${suffix} .
 	popd
 
 	pushd server
-	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/server_linux_arm$v  .
-	popd
-done
-pushd $OUT
-if $UPX; then upx -9 client_linux_arm* server_linux_arm*;fi
-tar -zcf kcptun-linux-arm-$VERSION.tar.gz client_linux_arm* server_linux_arm*
-$sum kcptun-linux-arm-$VERSION.tar.gz
-popd
-
-# ARMv8/ARM64/aarch64
-pushd client
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/client_linux_arm64  .
-popd
-
-pushd server
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/server_linux_arm64  .
-popd
-
-pushd $OUT
-if $UPX; then upx -9 client_linux_arm64 server_linux_arm64;fi
-tar -zcf kcptun-linux-arm64-$VERSION.tar.gz client_linux_arm64 server_linux_arm64
-$sum kcptun-linux-arm64-$VERSION.tar.gz
-popd
-
-
-
-#MIPS32LE
-MIPS=(mipsle mips)
-for v in ${MIPS[@]}; do
-	pushd client
-	env CGO_ENABLED=0 GOOS=linux GOARCH=$v GOMIPS=softfloat go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/client_linux_$v .
-	popd
-
-	pushd server
-	env CGO_ENABLED=0 GOOS=linux GOARCH=$v GOMIPS=softfloat go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/server_linux_$v .
+	startgo $v build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ../$OUT/server_${os}_${arch}${suffix} .
 	popd
 
 	pushd $OUT
-	if $UPX; then upx -9 client_linux_$v server_linux_$v;fi
-	tar -zcf kcptun-linux-$v-$VERSION.tar.gz client_linux_mips* client_linux_mips*
-	$sum kcptun-linux-$v-$VERSION.tar.gz
+	if $UPX; then upx -9 client_${os}_${arch}${suffix} server_${os}_${arch}${suffix};fi
+	tar -zcf kcptun-${os}-${arch}-$VERSION.tar.gz client_${os}_${arch}${suffix} server_${os}_${arch}${suffix}
+	$sum kcptun-${os}-${arch}-$VERSION.tar.gz
 	popd
 done
 
